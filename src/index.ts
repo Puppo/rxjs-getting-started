8141dfd5e53f02510875ffdb486315b25d208302
Luca Del Puppo <luca@delpuppo.net>
2021-07-21 15:54:05 +0200

    create observable

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log("Hello from RxJS Getting Started");
+import { Observable, Observer, Subscriber } from "rxjs";
+
+const observer: Observer<string> = {
+  next: (value: string) => console.log("next", value),
+  error: (error: Error) => console.error("error", error),
+  complete: () => console.log("complete!"),
+};
+
+const observable = new Observable<string>((subscriber: Subscriber<string>) => {
+  subscriber.next("Hello");
+  subscriber.next("World");
+
+  subscriber.complete();
+
+  // this will never be logged
+
+  subscriber.error(new Error("Something went wrong!"));
+
+  subscriber.next("Hello");
+  subscriber.next("World");
+});
+
+observable.subscribe(observer);

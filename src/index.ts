4e97d502aa145268dce6ac9a0d1c11fe449da1cd
Luca Del Puppo <luca@delpuppo.net>
2021-07-21 16:21:03 +0200

    operators

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-console.log("Hello from RxJS Getting Started");
+import { Observable, of } from "rxjs";
+import { map, tap } from "rxjs/operators";
+
+/**
+ * Creation Operations
+ */
+of(1, 2, 3, 4).subscribe(x => console.log("[creation] result", x));
+
+/**
+ * Pipeable Operators
+ */
+new Observable<number>(observer => {
+  observer.next(1);
+  observer.next(2);
+  observer.next(3);
+  observer.next(4);
+
+  observer.complete();
+})
+  .pipe(
+    map(val => val * 2),
+    tap(res => {
+      console.log("[pipeable tap]", res);
+    })
+  )
+  .subscribe(res => {
+    console.log("[pipeable] result", res);
+  });
